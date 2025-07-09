1934e8bc105421ee49d3a84838b2f08399da0789
95algfri <jakwanjaeger05@gmail.com>
2025-07-09 04:14:09 +0000

    # review and solve problem on script.js # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   script.js #

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const signUpBtn = document.getElementById('signUp');
+const signInBtn = document.getElementById('signIn');
 
-registerBtn.addEventListener('click', () => {
+signUpBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
 
-loginBtn.addEventListener('click', () => {
+signInBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
